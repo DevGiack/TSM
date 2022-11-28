1e6429600359199b568bb6875c157b7782a77dad
devgiack <devgiack@protonmail.com>
2022-11-28 15:36:30 +0100

    installation de divise OK et test OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'static_pages/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
