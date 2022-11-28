7ca55c6865a06367da4dafa5fcc5f17c2a412d22
devgiack <devgiack@protonmail.com>
2022-11-28 16:49:52 +0100

    Fin de l'installation de Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'static_pages/index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
