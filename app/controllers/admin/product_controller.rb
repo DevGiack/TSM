166f7a0ef26bfc2ce22db0a7bea96c04b90f11c6
Lucasss12 <lucas.mene.pro@gmail.com>
2022-12-01 00:25:28 +0100

    admin panel OK

--- app/controllers/admin/product_controller.rb
+++ app/controllers/admin/product_controller.rb
@@ -19,4 +19,23 @@ class Admin::ProductController < ApplicationController
             render inline: @products.errors.full_messages 
         end
     end
+
+    def edit
+        @products = Product.find(params[:id])
+    end
+
+    def update
+        @products = Product.find(params[:id])
+        if @products.update(name: params[:name], description: params[:description], unity: params[:unity], price: params[:price])   
+         redirect_to admin_product_path
+        else
+           render :edit
+        end
+    end
+
+    def destroy
+        @products = Product.find(params[:id])
+        @products.destroy
+        redirect_to admin_product_index_path
+    end
 end
