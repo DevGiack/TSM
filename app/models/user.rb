799d0f808180c6263583caf8553b6592a7ab188c
devgiack <devgiack@protonmail.com>
2022-12-01 06:39:09 +0100

    création + config du mail de bienvenue

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  after_create :welcome_send
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
 
 end
 
