1032176dbdce3dfc93d87e2129f56e9db9bc9cc8
adeloing <adeloing@gmail.com>
2022-12-02 12:43:29 +0100

    maj product controller + change variable name in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    highlighted_products = Product.where(is_highlighted: true)
+    @highlighted_products = Product.where(is_highlighted: true)
   end
 end
