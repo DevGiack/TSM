4dd795982650658432968f0bcb7e7c25282ed247
adeloing <adeloing@gmail.com>
2022-12-05 14:36:57 +0100

    remove create cart bis migration file

--- db/migrate/20221128151332_create_carts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarts < ActiveRecord::Migration[7.0]
-  def change
-    create_table :carts do |t|
-      t.integer :quantity
-      t.timestamps
-    end
-    add_reference :carts, :product, foreign_key: true
-  end
-end
