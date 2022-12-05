c06d9c5d4c4e7db93282deb4e53879b5967da02c
adeloing <adeloing@gmail.com>
2022-12-05 13:59:11 +0100

    add dynamic home + cart logic

--- db/migrate/20221128151554_create_orders.rb
+++ db/migrate/20221128151554_create_orders.rb
@@ -3,7 +3,6 @@ class CreateOrders < ActiveRecord::Migration[7.0]
     create_table :orders do |t|
       t.timestamps
     end
-    add_reference :orders, :cart, foreign_key: true
     add_reference :orders, :user, foreign_key: true
   end
 end
