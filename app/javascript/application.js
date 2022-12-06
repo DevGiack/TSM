92cae93c8863e8f90da0ca8a22e846cf6ad71adc
devgiack <devgiack@protonmail.com>
2022-12-06 16:14:47 +0100

    bannière navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,23 +2,26 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-$(document).ready(function() {
-      const minus = $('.quantity__minus');
-      const plus = $('.quantity__plus');
-      const input = $('.quantity__input');
-      minus.click(function(e) {
-        e.preventDefault();
-        var value = input.val();
-        if (value > 1) {
-          value--;
-        }
-        input.val(value);
-      });
+
+/* Button incrementation */
+
+// $(document).ready(function() {
+//       const minus = $('.quantity__minus');
+//       const plus = $('.quantity__plus');
+//       const input = $('.quantity__input');
+//       minus.click(function(e) {
+//         e.preventDefault();
+//         var value = input.val();
+//         if (value > 1) {
+//           value--;
+//         }
+//         input.val(value);
+//       });
       
-      plus.click(function(e) {
-        e.preventDefault();
-        var value = input.val();
-        value++;
-        input.val(value);
-      })
-    });
+//       plus.click(function(e) {
+//         e.preventDefault();
+//         var value = input.val();
+//         value++;
+//         input.val(value);
+//       })
+//     });
