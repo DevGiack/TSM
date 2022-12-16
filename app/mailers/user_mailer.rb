f8b59e1eafa6b63dd1946296bc66082b75e93c40
devgiack <devgiack@protonmail.com>
2022-12-16 15:17:58 +0100

    url maj

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
       @user = user 
 
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
-      @url  = 'https://truffieresaintmartin.herokuapp.com/' 
+      @url  = 'https://warm-shore-71821.herokuapp.com/' 
 
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @user.email, subject: 'Bienvenue chez les truffiéres de Saint-Martin !') 
